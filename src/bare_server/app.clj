c7dd892285722a188dedf450484130121f82c6cc
Antoine Chesnais <achesnais@gmail.com>
2016-07-26 19:38:35 +0200

    Added main function.

--- src/bare_server/app.clj
+++ src/bare_server/app.clj
@@ -5,3 +5,23 @@
 (defn build-app [config]
   (component/system-map
    :server (server/new config)))
+
+(def config
+  {:port 8080})
+
+(defn -main []
+  (println "Starting app....")
+  (let [app (component/start (build-app config))]
+    (Thread/sleep 20000)
+    (println "Stopping app!")
+    (component/stop app)))
+
+(comment
+
+  (def app (build-app {:port 8080}))
+  (def started-app (component/start app))
+  (def stopped-app (component/stop started-app))
+
+  (-main)
+
+  )
